5f33f178428070074fcd1fbe5ac86bb3db76bcb7
Jorge Miguel Werle Junior <jmwjunior@hotmail.com>
2022-11-17 11:35:26 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import NotificantionButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Vendas</h1>
+    <>
+      <h1>Vendas</h1>
+      <NotificantionButton />
+      <NotificantionButton />
+      <NotificantionButton /> 
+      <NotificantionButton />
+    </>
   )
 }
 
