a84e9627ab37c7d07a7b2e0edd365833659eeb21
Jorge Miguel Werle Junior <jmwjunior@hotmail.com>
2022-11-17 12:50:03 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
-import NotificantionButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Vendas</h1>
-      <NotificantionButton />
-      <NotificantionButton />
-      <NotificantionButton /> 
-      <NotificantionButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+
     </>
   )
 }
